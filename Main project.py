616500d7270dcfa1896eae117dc482164d9b8144
MarleyGG5 <marleygladmangrace8@gmail.com>
2025-01-29 14:58:15 +0000

    Finished VON main

--- Main project.py
+++ Main project.py
@@ -3,7 +3,7 @@ import subprocess
 run = True
 while run:
 
-    menu = input('What would you like to do: \n 1) Convert to different resolution \n 2) Change format \n 3) Change the file type \n Q) Exit the programme \n')
+    menu = input('What would you like to do: \n 1) Convert to different resolution \n 2) Change format \n 3) Change the file type \n 4) Stream video \n Q) Exit the programme \n')
 
     if menu == 'Q':
         confirm = input('Are you sure you would like to quit the programme [Y/N] ')
@@ -37,13 +37,17 @@ while run:
     
     if menu == '4':
         filen = input('What file would you like to stream: ')
-        ipaddr = input('Enter the ip destination e.g (192.168.2.1): ')
-        port = input('Enter port to stream to e.g (5004): ')
-
-        subprocess.run(['ffmpeg', '-i', filen, '-f', 
-                        'rtp', '-vcodec', 'libx264', '-acodec', 
-                        'aac', '-bufsize', '2000k', '-g', '50', f'rtp://{ipaddr}:{port}'
-                        ])
-
-
-        # to stream on reciving end ffplay rtp://<ip_address>:<Port>
+        ipaddr = input('Enter the destination IP address (e.g., 127.0.0.1): ')
+        port = input('Enter the port to stream to (e.g., 5000): ')
+        
+        # Stream over UDP
+        subprocess.run([
+            'ffmpeg',
+            '-i', filen,                        # Input file
+            '-c:v', 'libx264',                  # Video codec
+            '-c:a', 'aac',                      # Audio codec
+            '-f', 'mpegts',                     # MPEG Transport Stream format
+            f'udp://{ipaddr}:{port}'            # Output stream destination
+        ])
+        
+        print(f"Streaming to udp://{ipaddr}:{port}")
